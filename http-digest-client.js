926aee4b56fc8e38bf326a8ddb7714ee16c855ed
shubham <shubham@cronj.com>
2020-10-21 22:56:46 +0530

    added error handling and baisc auth support

--- http-digest-client.js
+++ http-digest-client.js
@@ -36,56 +36,61 @@ var HTTPDigest = function () {
   // Parse authentication headers and set response.
   //
   HTTPDigest.prototype._handleResponse = function handleResponse(options, res, callback) {
-    var challenge = this._parseChallenge(res.headers['www-authenticate']);
-    var ha1 = crypto.createHash('md5');
-    ha1.update([this.username, challenge.realm, this.password].join(':'));
-    var ha2 = crypto.createHash('md5');
-    ha2.update([options.method, options.path].join(':'));
+    try{
+      var challenge = this._parseChallenge(res.headers['www-authenticate']);
+      var ha1 = crypto.createHash('md5');
+      ha1.update([this.username, challenge.realm, this.password].join(':'));
+      var ha2 = crypto.createHash('md5');
+      ha2.update([options.method, options.path].join(':'));
 
-    // Generate cnonce
-    var cnonce = false;
-    var nc = false;
-    if (typeof challenge.qop === 'string') {
-      var cnonceHash = crypto.createHash('md5');
-      cnonceHash.update(Math.random().toString(36));
-      cnonce = cnonceHash.digest('hex').substr(0, 8);
-      nc = this.updateNC();
+      // Generate cnonce
+      var cnonce = false;
+      var nc = false;
+      if (typeof challenge.qop === 'string') {
+        var cnonceHash = crypto.createHash('md5');
+        cnonceHash.update(Math.random().toString(36));
+        cnonce = cnonceHash.digest('hex').substr(0, 8);
+        nc = this.updateNC();
+      }
+
+      // Generate response hash
+      var response = crypto.createHash('md5');
+      var responseParams = [
+        ha1.digest('hex'),
+        challenge.nonce
+      ];
+
+      if (cnonce) {
+        responseParams.push(nc);
+        responseParams.push(cnonce);
+      }
+
+      responseParams.push(challenge.qop);
+      responseParams.push(ha2.digest('hex'));
+      response.update(responseParams.join(':'));
+
+      // Setup response parameters
+      var authParams = {
+        username: this.username,
+        realm: challenge.realm,
+        nonce: challenge.nonce,
+        uri: options.path,
+        qop: challenge.qop,
+        response: response.digest('hex'),
+        opaque: challenge.opaque
+      };
+      if (cnonce) {
+        authParams.nc = nc;
+        authParams.cnonce = cnonce;
+      }
+
+      var headers = options.headers || {};
+      headers.Authorization = this._compileParams(authParams);
+      options.headers = headers;
     }
-
-    // Generate response hash
-    var response = crypto.createHash('md5');
-    var responseParams = [
-      ha1.digest('hex'),
-      challenge.nonce
-    ];
-
-    if (cnonce) {
-      responseParams.push(nc);
-      responseParams.push(cnonce);
+    catch (err) {
+      console.log("Not a digest auth api")
     }
-
-    responseParams.push(challenge.qop);
-    responseParams.push(ha2.digest('hex'));
-    response.update(responseParams.join(':'));
-
-    // Setup response parameters
-    var authParams = {
-      username: this.username,
-      realm: challenge.realm,
-      nonce: challenge.nonce,
-      uri: options.path,
-      qop: challenge.qop,
-      response: response.digest('hex'),
-      opaque: challenge.opaque
-    };
-    if (cnonce) {
-      authParams.nc = nc;
-      authParams.cnonce = cnonce;
-    }
-
-    var headers = options.headers || {};
-    headers.Authorization = this._compileParams(authParams);
-    options.headers = headers;
 
     http.request(options, function (res) {
       callback(res);
